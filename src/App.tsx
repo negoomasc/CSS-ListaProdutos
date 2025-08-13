f27d9b14b7f9a87444c2b586c8dfc946f09181f3
NorevendeLee <norevende.lee@gmail.com>
2025-08-13 11:24:27 -0300

    versao 0.0.2 com footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <>
-    <nav className="navbar">
-      <h1>Meu Site</h1>
-      <ul>
-        <li><a href="#">Início</a></li>
-        <li><a href="#">Serviços</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contato</a></li>
-      </ul>
-    </nav>
+      <nav className="navbar">
+        <h1>Meu Site</h1>
+        <ul>
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Serviços</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+
+      <main>
+        {/* Conteúdo da página */}
+      </main>
+
+      <footer className="footer">
+        <p>© 2025 Minha Empresa</p>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">LinkedIn</a></li>
+        </ul>
+      </footer>
     </>
   );
 }
