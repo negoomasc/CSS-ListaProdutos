14ab44eeba78e14f3507b8daea80d6d705b5355a
NorevendeLee <norevende.lee@gmail.com>
2025-08-13 11:15:38 -0300

    Menu de navegação versao 0.0.1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+    <nav className="navbar">
+      <h1>Meu Site</h1>
+      <ul>
+        <li><a href="#">Início</a></li>
+        <li><a href="#">Serviços</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </nav>
+    </>
+  );
+}
